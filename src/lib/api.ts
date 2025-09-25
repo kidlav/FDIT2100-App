4d945df5c5326e530dd0b545461a75a6dcbd0203
Vladislav Kovalenko <vladislav@Mac-mini-Vladislav.local>
2025-09-25 10:27:10 -0700

    Post and Tag component

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { type PostResponse } from "@/lib/types/post";
-import { API_BASE_URL } from "./constants";
+import { API_BASE_URL, POSTS_PER_PAGE } from "@/lib/constants";
 
-export function fetchPosts() {
-    return axios.get<PostResponse>(`${API_BASE_URL}/posts`)
+export function fetchPosts(page: number) {
+    return axios.get<PostResponse>(`${API_BASE_URL}/posts?limit=${POSTS_PER_PAGE}&skip=${page * POSTS_PER_PAGE}`)
     .then(response => response.data)
 }
