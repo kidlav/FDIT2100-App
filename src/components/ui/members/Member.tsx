8a166415c214e45c7d8e8d5815afcc3973bed281
Vladislav Kovalenko <vladislav@Mac-mini-Vladislav.local>
2025-10-16 11:31:41 -0700

    x icon to clear search field

--- src/components/ui/members/Member.tsx
+++ src/components/ui/members/Member.tsx
@@ -13,18 +13,19 @@ export default function Member(props: Props) {
     const navigate = useNavigate();
 
     return(
-        <li key={member.id} className='flex mb-8'>
-            <div className='mr-4 mb-8'>
+        <li key={member.id} className=' flex mb-8'>
+            <div className=' mr-4 mb-8'>
                 <h3 className='mb-1 text-lg font-semibold'>
                     {`${member.firstName} ${member.lastName}`}
                 </h3>
                 <p className={styles.memberBio}>
                     {`${member.address.city}, ${member.address.country} | ${member.company.name} | ${member.company.title}`} 
                 </p>
+                <img src={`/avatars/${member.id}.png`} alt={`${member.firstName} ${member.lastName}'s avatar`} className='block md:hidden aspect-[7/5] h-42 object-cover ml-[30%] rounded-lg'></img>
                 <Button className={styles.viewPostsButton} onClick={() => navigate(`/members/${member.id}/posts`)}>View Posts</Button>
             </div>
-            <img src={`/avatars/${member.id}.png`} alt={`${member.firstName} ${member.lastName}'s avatar`} className='aspect-[7/4] h-42 object-cover ml-auto rounded-lg'></img>
+            <img src={`/avatars/${member.id}.png`} alt={`${member.firstName} ${member.lastName}'s avatar`} className='hidden md:block aspect-[7/4] h-42 object-cover ml-auto rounded-lg'></img>
 
         </li>
-    )
+    );
 }
--- src/components/ui/members/MembersList.tsx
+++ src/components/ui/members/MembersList.tsx
@@ -1,40 +1,75 @@
-import { useSuspenseQuery} from "@tanstack/react-query";
-import Member from "@/components/ui/members/member";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import Member from "@/components/ui/members/Member";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 import { fetchMembers } from "@/lib/api";
 import { Input } from '@/components/ui/input';
 import styles from "./membersList.module.css"
-import {useCallback, useState } from "react";
-import { type Member as MemberType} from "@/lib/types/member";
+import { useMemo, useState } from "react";
+import { type Member as MemberType } from "@/lib/types/member";
+import { XMarkIcon } from "@heroicons/react/24/solid";
 
 
 
 export default function MembersList() {
     const [nameFilter, setNameFilter] = useState('');
 
-    const filterMembers = useCallback((member: MemberType) => {
-        return member.firstName.toLowerCase().includes(nameFilter.toLowerCase()) ||
-        member.lastName.toLowerCase().includes(nameFilter.toLowerCase())
-    }, [nameFilter]);
-    
     const { data } = useSuspenseQuery({
         queryKey: ['members'],
         queryFn: () => fetchMembers(),
         staleTime: 1000 * 60 * 5,
     });
-    
+
+    const filteredMembers = useMemo(
+        () =>
+          data.users.filter((member: MemberType) => {
+            const fullName = `${member.firstName} ${member.lastName}`.toLowerCase();
+            const reversedName = `${member.lastName} ${member.firstName}`.toLowerCase();
+            const query = nameFilter.toLowerCase().trim();
+
+        return fullName.includes(query) || reversedName.includes(query);
+        }),
+        [data.users, nameFilter]
+      );
+
+
     return (
         <>
+            <div className="relative">
+                <Input name="search-members" 
+                value={nameFilter} 
+                onChange={event => setNameFilter(event.target.value)} 
+                placeholder="Search Members" 
+                className={styles.searchField} />
+                
+                <MagnifyingGlassIcon className="size-5 -translate-y-8 translate-3 text-gray-400" />
 
-        <Input name="search-members" value={nameFilter} onChange={event => setNameFilter(event.target.value)} placeholder="Search members" className={styles.searchField} />
-        <MagnifyingGlassIcon className="size-5 -translate-y-8 translate-3 text-gray-400" />
-        <ul className="mt-3">
-            {data.users
-            .filter(filterMembers)
-            .map(user => (
-                <Member key={user.id} member={user}/>
-            ))}
-        </ul>   
+                <ul className="mt-3">
+                    {filteredMembers.length == 0 ? (
+                        <li className=" text-gray-500">No member found 😢</li>
+                    ) :
+                        filteredMembers.map(user => (
+                            <Member key={user.id} member={user} />
+                        ))}
+                    {nameFilter && (
+                        <XMarkIcon
+                            onClick={() => setNameFilter("")}
+                            className="size-5 absolute right-5 top-3 cursor-pointer text-gray-400 hover:text-gray-600"
+                        />
+                    )}
+                </ul>
+            </div>
         </>
     );
 }
+
+
+{/* {data.users
+                .filter(filteredMembers)
+                .map(user => (
+                <Member key={user.id} member={user}/>
+            ))} */}
+
+
+{/* {data.users.filter(filterMembers).length === 0 && (
+                <p className="text-center text-2xl text-gray-500 mt-4">No member found 😢</p>
+            )} */}
